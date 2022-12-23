0d195793da04a5a5af98acd7385eaca00f77f32d
Jennifer Blacutt <jennifer@blacutt.com>
2022-12-23 15:53:57 -0600

    add login and signup js

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  
+  
+document.querySelector('#login-btn').addEventListener('submit', loginFormHandler);
