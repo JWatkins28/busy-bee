8f04a4049946988fa2b1bea7a1176227cc938526
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2022-12-22 23:02:27 -0600

    fixing server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const taskRoutes = require('./goalRoutes');
+const taskRoutes = require('./taskRoutes');
 const subtaskRoutes = require('./subtaskRoutes')
 
 router.use('/users', userRoutes);
-router.use('/tasks', taskRoutes);
-router.use('/subtasks', subtaskRoutes);
+// router.use('/tasks', taskRoutes);
+// router.use('/subtasks', subtaskRoutes);
 
 module.exports = router;
